f4141b09c72bc0e8b22d3f5d21ff424473174388
liuqh <302728910@qq.com>
2026-01-02 17:17:43 +0800

    复现最小-循环依赖的问题

--- src/test/java/org/spring/test/v1/service/UserRepository.java
+++ src/test/java/org/spring/test/v1/service/UserRepository.java
@@ -1,6 +1,11 @@
 package org.spring.test.v1.service;
 
+import org.spring.v1.Autowired;
+
 public class UserRepository {
+
+    @Autowired
+    private UserService userService;
 
     public void selectById(){
         System.out.println("select 1 from user where id = 1");
